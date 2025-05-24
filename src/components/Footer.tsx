2e528e77cd4b1691b354ee36f0aa3e63351788e1
Amey Dabhade <ameydabhade4@gmail.com>
2025-05-25 01:12:00 +0530

    map

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,8 +29,17 @@ export default function Footer() {
       </div>
 
       {/* Google Maps Embed */}
-      <div className="mt-6 bg-gray-200 rounded-lg h-48 flex items-center justify-center">
-        <span className="text-gray-500">Google Maps Embed</span>
+      <div className="mt-6 bg-gray-200 rounded-lg overflow-hidden" style={{ height: '300px' }}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.090006682008!2d73.73218809999999!3d18.659956600000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b10fc5788b8b%3A0xa78bb2decc4efca1!2sDr%20Rudagi's%20Dental%20Centre%20Of%20Xcellence!5e0!3m2!1sen!2sin!4v1748115653813!5m2!1sen!2sin"
+          width="100%"
+          height="100%"
+          style={{ border: 0 }}
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Dr. Rudagi's Dental Centre Location"
+        ></iframe>
       </div>
     </footer>
   );
